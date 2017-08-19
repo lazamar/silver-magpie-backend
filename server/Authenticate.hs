3f01b9684552fa9472655dc86cbd63ee271a2d20
Marcelo Lazaroni <lazaronijunior@gmail.com>
2017-08-19 17:18:20 +0100

    Refactor authenticate

--- server/Authenticate.hs
+++ server/Authenticate.hs
@@ -1,31 +1,48 @@
+{-# LANGUAGE RankNTypes #-}
+
 module Authenticate (authenticate) where
 
-import MongoTypes.UserDetails (UserDetails (UserDetails))
-import Types (DBActionRunner)
 
-authenticate :: DBActionRunner -> String -> IO (Maybe UserDetails)
-authenticate runDbAction sessionId =
-    runDbAction $ getUserDetailsFromSessionId sessionId
+import Control.Monad.IO.Class (MonadIO)
+import Data.Bson (Document, (=:))
+import Data.Either.Combinators (rightToMaybe)
+import Database.MongoDB.Query (Action, Collection, Selector, findOne, select)
+import MongoTypes.AppAuth (AppAuth)
+import qualified MongoTypes.AppAuth as AppAuth
+import MongoTypes.UserDetails (UserDetails)
+import qualified MongoTypes.UserDetails as UserDetails
 
 
-getUserDetailsFromSessionId :: MonadIO m => String -> Action m (Maybe UserDetails)
-getUserDetailsFromSessionId sessionId =
+authenticate :: MonadIO m => String -> Action m (Maybe UserDetails)
+authenticate sessionId =
     do
-        mToken <- (accessRequestToken =<<) <$> getRequestTokenBySessionId sessionId
-        case mToken of
+        mAppAuth <- getAppAuthBySessionId sessionId
+        case mAppAuth of
             Nothing ->
                 return Nothing
-            Just token ->
-                getUserDetailsByRequestToken token
-    where
-        accessRequestToken doc =
-            Bson.lookup "access_request_token" doc ::Maybe String
+            Just appAuth ->
+                getUserDetailsByRequestToken $ AppAuth.accessRequestToken appAuth
 
-getRequestTokenBySessionId :: MonadIO m => String -> Action m (Maybe AppAuth)
-getRequestTokenBySessionId sessionId =
-    let
-        selector = [ "app_session_id" =: sessionId ]
-        collection = "app-authorisation"
-        selection = Mongo.select selector collection
-    in
-        findOne selection
+
+getAppAuthBySessionId :: MonadIO m => String -> Action m (Maybe AppAuth)
+getAppAuthBySessionId sessionId =
+    fetch
+        [ AppAuth.keyAppSessionId =: sessionId ]
+        AppAuth.collectionName
+        AppAuth.fromBSON
+
+
+getUserDetailsByRequestToken :: MonadIO m => String -> Action m (Maybe UserDetails)
+getUserDetailsByRequestToken token =
+    fetch
+        [ UserDetails.keyAccessRequestToken =: token ]
+        UserDetails.collectionName
+        UserDetails.fromBSON
+
+
+fetch :: MonadIO m => Selector -> Collection -> (Document -> Either e a) -> Action m (Maybe a)
+fetch selector collection decoder =
+    (=<<) (rightToMaybe . decoder) <$> findOne selection
+    where
+        selection =
+            select selector collection
