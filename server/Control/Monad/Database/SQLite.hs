238fcf4b743f55217ae8c78f3ca34a38637f9058
Marcelo Lazaroni <lazaronijunior@gmail.com>
2021-01-10 20:21:30 +0000

    Implement MonadDB version of MySQL

--- server/Control/Monad/Database/SQLite.hs
+++ server/Control/Monad/Database/SQLite.hs
@@ -3,6 +3,8 @@
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeFamilies #-}
 
 module Control.Monad.Database.SQLite where
 
@@ -13,7 +15,9 @@ import Control.Monad.Conc.Class (MonadConc)
 import Control.Monad.IO.Class (MonadIO, liftIO)
 import Control.Monad.Reader (ReaderT (..), ask, local, reader, runReaderT)
 import Control.Monad.Trans.Class (MonadTrans, lift)
-import Database.SQLite.Simple (FromRow, NamedParam, Query, ToRow)
+import Data.List (intercalate)
+import Data.String (fromString)
+import Database.SQLite.Simple (FromRow, NamedParam, Query, SQLData, ToRow)
 import qualified Control.Monad.Database as DB
 import qualified Data.Bson as Bson
 import qualified Data.Text as Text
@@ -23,6 +27,7 @@ import qualified Database.SQLite.Simple as SQL
 -- | Access an SQL database
 class Monad m => MonadSQL m where
     execute :: ToRow q => Query -> q -> m ()
+    executeNamed :: Query -> [NamedParam] -> m ()
     execute_ :: Query -> m ()
     query :: (ToRow q , FromRow r) => Query -> q -> m [r]
     query_ :: (FromRow r) => Query -> m [r]
@@ -46,6 +51,7 @@ data DBState m = DBState
 
 instance (MonadConc m , MonadMask m , MonadIO m) => MonadSQL (MonadSQLT m) where
     execute q v = MonadSQLT $ write $ \conn -> liftIO $ SQL.execute conn q v
+    executeNamed q v = MonadSQLT $ write $ \conn -> liftIO $ SQL.executeNamed conn q v
     execute_ q = MonadSQLT $ write $ \conn -> liftIO $ SQL.execute_ conn q
     query q v = MonadSQLT $ reader conn >>= \conn -> liftIO $ SQL.query conn q v
     query_ q = MonadSQLT $ reader conn >>= \conn -> liftIO $ SQL.query_ conn q
@@ -80,12 +86,60 @@ write action = do
         False -> error "Trying to write after transaction was closed"
         True -> action conn
 
-----
+instance forall m. (MonadConc m , MonadMask m , MonadIO m) => DB.MonadDB (MonadSQLT m) where
+    type Record (MonadSQLT m) = [SQLData]
+    type Value (MonadSQLT m) = SQLData
 
--- instance DB.MonadDB (MonadSQLT m) where
---     type Record (MonadSQLT m) = [SQL.SQLData]
---     type Value (MonadSQLT m) = String
---
---     store target = undefined
---     retrieve target = undefined
---     delete target = undefined
+    store (DB.Target _ (DB.Collection col)) = \case
+        DB.Insert val -> do
+            let record = DB.toRecord @(MonadSQLT m) val
+            execute
+                (fromString $ "INSERT OR REPLACE INTO " <> col <> "VALUES (" <> queryRep record <> ")")
+                record
+        DB.Update q val -> do
+            -- check whether the data in q is primary key, if not throw not error
+            let record = DB.toRecord @(MonadSQLT m) val
+            execute
+                (fromString $ "INSERT OR REPLACE INTO " <> col <> "VALUES (" <> queryRep record <> ")")
+                record
+
+    retrieve (DB.Target _ (DB.Collection col)) = \case
+        DB.SelectOne fields -> do
+            let vars = zip varNames fields
+                q =
+                    fromString $
+                        unwords
+                            [ "SELECT * FROM " <> col
+                            , "WHERE " <> unwords (map asFieldQuery vars)
+                            , "LIMIT 1"
+                            ]
+
+            records <- queryNamed q (map asNamedParam vars)
+            return $ fmap (DB.fromRecord @(MonadSQLT m)) records
+
+    delete (DB.Target _ (DB.Collection col)) = \case
+        DB.DeleteMany fields -> do
+            let vars = zip varNames fields
+                q =
+                    fromString $
+                        unwords
+                            [ "DELETE FROM " <> col
+                            , "WHERE " <> unwords (map asFieldQuery vars)
+                            ]
+            executeNamed q (map asNamedParam vars)
+
+queryRep :: [SQLData] -> String
+queryRep xs = intercalate "," $ replicate (length xs) "?"
+
+asFieldQuery :: (VarName , DB.FieldValue m) -> String
+asFieldQuery (VarName varName , DB.FieldValue field _) = field <> " = " <> varName
+
+asNamedParam :: forall m. (VarName , DB.FieldValue (MonadSQLT m)) -> NamedParam
+asNamedParam (VarName varName , DB.FieldValue _ val) =
+    Text.pack varName SQL.:= DB.toValue @(MonadSQLT m) val
+
+newtype VarName = VarName String
+
+-- | Placeholder names for searches
+varNames :: [VarName]
+varNames = [VarName $ ":" <> pure letter | letter <- ['a' .. 'z']]
