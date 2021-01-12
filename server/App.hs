f5ba4c6a1e430de8532bff5bca36f43e81a328af
Marcelo Lazaroni <lazaronijunior@gmail.com>
2021-01-12 23:59:10 +0000

    Add table creations

--- server/App.hs
+++ server/App.hs
@@ -15,6 +15,7 @@ import Control.Monad.Conc.Class (MonadConc)
 import Control.Monad.Database.SQLite
 import Control.Monad.Except (ExceptT (..))
 import Data.Bool (bool)
+import Data.String (fromString)
 import Network.HTTP.Client (Manager)
 import Network.HTTP.Client.TLS (newTlsManager)
 import Network.Wai (Middleware)
@@ -43,6 +44,7 @@ import Types
     ( EnvironmentVariables (dbName , dbPassword , dbPort , dbUrl , dbUsername , port),
       HandlerM,
     )
+import qualified Control.Monad.Database.SQLite as SQL
 import qualified Data.AppAuth as AppAuth
 import qualified Data.Text as T
 import qualified Data.UserDetails as UserDetails
@@ -55,7 +57,7 @@ runApp :: EnvironmentVariables -> IO ()
 runApp env = do
     putStrLn $ "Running server on port " ++ show (port env)
 
-    let S runMonad = stackRunner env
+    let S runMonad = databaseRunner env
 
     -- SERVER START
     manager <- newTlsManager
@@ -65,16 +67,50 @@ runApp env = do
 
 type Stack = MonadSQLT (ExceptT ServerError IO)
 
-newtype StackRunner m n = S (forall a. m a -> n a)
+newtype DBRunner m n = S (forall a. m a -> n a)
 
 -- TODO database name must be set by the environment variables
-stackRunner :: EnvironmentVariables -> StackRunner Stack Handler
-stackRunner _ = S $ Handler . runMonadSQL "./database/DB.sql"
+databaseRunner :: EnvironmentVariables -> DBRunner Stack Handler
+databaseRunner _ = S $ \runServer -> Handler $
+    runMonadSQL "./database/DB.sql" $ do
+        prepareDatabase
+        runServer
     where
         collections =
             [ UserDetails.collectionName
             , AppAuth.collectionName
             ]
+
+        prepareDatabase = do
+            -- Prepare database for usage
+            -- Enable foreign key constraints. It's really weird that they would otherwise just not work.
+            SQL.execute_ "PRAGMA foreign_keys = ON"
+            -- Make sure the correct transaction tracking option is in place
+            SQL.execute_ "PRAGMA journal_mode = WAL"
+            SQL.execute_ "PRAGMA synchronous = NORMAL"
+            createTables
+
+        createTables = do
+            SQL.execute_ $
+                asQuery
+                    [ "CREATE TABLE IF NOT EXISTS " <> AppAuth.collectionName <> " "
+                    , "( " <> AppAuth.keyAppSessionId <> "         TEXT NOT NULL"
+                    , ", " <> AppAuth.keyAccessRequestToken <> "   TEXT NOT NULL"
+                    , ")"
+                    ]
+
+            SQL.execute_ $
+                asQuery
+                    [ "CREATE TABLE IF NOT EXISTS " <> UserDetails.collectionName <> " "
+                    , "( " <> UserDetails.keyOauthToken <> "         TEXT NOT NULL"
+                    , ", " <> UserDetails.keyOauthTokenSecret <> "   TEXT NOT NULL"
+                    , ", " <> UserDetails.keyUserId <> "             TEXT NOT NULL"
+                    , ", " <> UserDetails.keyScreenName <> "         TEXT NOT NULL"
+                    , ", " <> UserDetails.keyAccessRequestToken <> " TEXT NOT NULL"
+                    , ", PRIMARY KEY (CHANNEL, COMMIT_HASH, REPRESENTS_DATE)"
+                    , ")"
+                    ]
+        asQuery = fromString . T.unpack . T.unwords
 
 app
     :: ()
